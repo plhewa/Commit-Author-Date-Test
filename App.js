d23fe9923637395047292fe14b29176b9ab20d3d
nuwana24 <nuwana24@gmail.com>
2022-10-05 20:23:02 +0530

    New fibonacci JS added

--- App.js
+++ App.js
@@ -1,22 +1,20 @@
+function fibonacci(num) {
+    if(num < 2) {
+        return num;
+    }
+    else {
+        return fibonacci(num-1) + fibonacci(num - 2);
+    }
+}
 
+// take nth term input from the user
+const nTerms = prompt('Enter the number of terms: ');
 
-
-
-//This is testing
-//fgfdgfdg
-
-//This is testing
-
-//fgdfgdfg
-
-//fdgdgd
-
-
-
-//fdgdsg
-
-//fdhsjfdskfhafhalfkdjshflasfalh
-
-//kkkkk
-
-//kkmkkjkjn
+if(nTerms <=0) {
+    console.log('Enter a positive integer.');
+}
+else {
+    for(let i = 0; i < nTerms; i++) {
+        console.log(fibonacci(i));
+    }
+}
